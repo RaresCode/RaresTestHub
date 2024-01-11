2633763dd1d96254b13bb5152b2d97399ff6b391
Rarescode <irimusrares7@gmail.com>
2024-01-11 22:12:28 +0200

    Fixed cities for atpgroup

--- sites/atpgroup.py
+++ sites/atpgroup.py
@@ -30,7 +30,6 @@ class atpgroupScraper(BS4Scraper):
         """
 
         job_elements = self.get_jobs_elements('css_', "div.elementor-element.elementor-element-63afff4.elementor-widget.elementor-widget-theme-post-title.elementor-page-title.elementor-widget-heading > div > h3 > a")
-        # job_location_elements = self.get_jobs_elements('class_', 'elementor-post-info__terms-list-item')
         job_location_elements = self.get_jobs_elements('class_', 'elementor-icon-list-items elementor-post-info')
         
         self.job_cities = []
@@ -42,7 +41,6 @@ class atpgroupScraper(BS4Scraper):
                 self.job_cities.append(city.split("Companie:")[0].replace("Locație: ", ""))
         
         self.job_titles = self.get_jobs_details_text(job_elements)
-        # self.job_cities = self.get_jobs_details_text(job_location_elements)
         self.job_urls = self.get_jobs_details_href(job_elements)
 
         self.format_data()
@@ -59,7 +57,20 @@ class atpgroupScraper(BS4Scraper):
         """
         Iterate over all job details and send to the create jobs dictionary.
         """
+        regiune_sud = ['Ilfov', 'Ploiești', 'Pitești']
+        regiune_sud_vest = ['Dolj', 'Olt', 'Vâlcea', 'Mehedinți']
+        regiune_vest = ['Bihor', 'Arad', 'Timișoara']
+        
         for job_title, job_url, job_city in zip(self.job_titles, self.job_urls, self.job_cities):
+            if "Vest" in job_title:
+                job_city = regiune_vest
+            elif "Sud-Vest" in job_title:
+                job_city = regiune_sud_vest
+            elif "Sud" in job_title:
+                job_city = regiune_sud
+            else:
+                job_city = job_city[:-1]
+                
             self.create_jobs_dict(job_title, job_url, "România", job_city)
 
 if __name__ == "__main__":
