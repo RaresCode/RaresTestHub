4eeb1b6c4cab3a2e098b59d23cc4cc12859712e7
Rarescode <irimusrares7@gmail.com>
2024-01-12 22:45:45 +0200

    Fixed job links for intelligentbee

--- sites/intelligentbee.py
+++ sites/intelligentbee.py
@@ -49,7 +49,7 @@ class intelligentbeeScraper(BS4Scraper):
         Iterate over all job details and send to the create jobs dictionary.
         """
         for job_title, job_url in zip(self.job_titles, self.job_urls):
-            job_url = "https://intelligentbee.com/" + job_url
+            job_url = "https://intelligentbee.com" + job_url
             self.create_jobs_dict(job_title, job_url, "România", "Iasi")
 
 if __name__ == "__main__":
