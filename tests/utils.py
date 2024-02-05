7d8f7db7c020c23aa1a6ca3139e155aba0dc04d7
Rarescode <irimusrares7@gmail.com>
2024-02-05 21:56:23 +0200

    Added main test assertion methods

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,5 @@
 import requests
+import allure
 
 class TestUtils:
 
@@ -71,3 +72,97 @@ class TestUtils:
             response_data = TestUtils._get_request(params)
 
         return all_future_title, all_future_job_city, all_future_job_country, all_future_job_link
+    
+    # Utility function for checking missing items
+    def get_missing_items(self, list_a, list_b):
+        return [item for item in list_a if item not in list_b][:20]
+
+    # Check method for job titles
+    def check_job_titles(self, expected_titles, actual_titles):
+        missing_titles = self.get_missing_items(expected_titles, actual_titles)
+
+        if missing_titles:
+            msg = f"Peviitor is having extra job titles: {missing_titles}"
+        else:
+            missing_titles = self.get_missing_items(actual_titles, expected_titles)
+            msg = f"Peviitor is missing job titles: {missing_titles}"
+
+        if not expected_titles and not actual_titles:
+            msg = f"Scraper is not grabbing any job titles"
+            allure.step(msg)
+            raise AssertionError(msg)
+        
+        allure.step(msg)
+        assert expected_titles == actual_titles, msg
+
+    # Check method for job cities
+    def check_job_cities(self, expected_cities, actual_cities):
+        missing_cities = self.get_missing_items(expected_cities, actual_cities)
+
+        if missing_cities:
+            msg = f"Peviitor is having extra job cities: {missing_cities}"
+        else:
+            missing_cities = self.get_missing_items(actual_cities, expected_cities)
+            msg = f"Peviitor is missing job cities: {missing_cities}"
+
+        if not expected_cities and not actual_cities:
+            msg = f"Scraper is not grabbing any job cities"
+            allure.step(msg)
+            raise AssertionError(msg)
+
+        allure.step(msg)
+        assert expected_cities == actual_cities, msg
+
+    # Check method for job countries
+    def check_job_countries(self, expected_countries, actual_countries):
+        missing_countries = self.get_missing_items(expected_countries, actual_countries)
+
+        if missing_countries:
+            msg = f"Peviitor is having extra job countries: {missing_countries}"
+        else:
+            missing_countries = self.get_missing_items(actual_countries, expected_countries)
+            msg = f"Peviitor is missing job countries: {missing_countries}"
+
+        if not expected_countries and not actual_countries:
+            msg = f"Scraper is not grabbing any job countries"
+            allure.step(msg)
+            raise AssertionError(msg)
+
+        allure.step(msg)
+        assert expected_countries == actual_countries, msg
+
+    # Check method for job links
+    def check_job_links(self, expected_links, actual_links):
+        missing_links = self.get_missing_items(expected_links, actual_links)
+
+        if missing_links:
+            msg = f"Peviitor is having extra job links: {missing_links}"
+        else:
+            missing_links = self.get_missing_items(actual_links, expected_links)
+            msg = f"Peviitor is missing job links: {missing_links}"
+        
+        if not expected_links and not actual_links:
+            msg = f"Scraper is not grabbing any job links"
+            allure.step(msg)
+            raise AssertionError(msg)
+
+        allure.step(msg)
+        assert expected_links == actual_links, msg
+
+    # Check method for job links
+    def check_code_job_links(self, status_codes_expected_result, status_codes_actual_result):
+        http_codes = self.get_missing_items(status_codes_expected_result, status_codes_actual_result)
+        msg = ""
+
+        if not http_codes:
+            msg = f"Some job links from scraper do not return 200 http status code: {http_codes}"
+            allure.step(msg)
+        
+        if not status_codes_expected_result and not status_codes_actual_result:
+            msg = f"Scraper is not grabbing any job links"
+            allure.step(msg)
+            raise AssertionError(msg)
+
+        if not msg:
+            msg = f"An unexpected error occured {status_codes_expected_result} {status_codes_actual_result}"
+        assert status_codes_expected_result == status_codes_actual_result, msg
