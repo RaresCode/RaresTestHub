3efbe0e09958c8956e64cbd2ac9f89a25fc95f40
Rarescode <irimusrares7@gmail.com>
2024-04-13 16:46:55 +0300

    Fixed britishcouncil job urls

--- sites/britishcouncil.py
+++ sites/britishcouncil.py
@@ -30,8 +30,15 @@ class britishcouncilScraper(BS4Scraper):
         """
 
         job_elements = self.get_jobs_elements('class_', 'bc-show-hide-title')
+        job_url_elements = self.get_jobs_elements('css_', 'div > div > p > strong > a')
         
         self.job_titles = self.get_jobs_details_text(job_elements)
+        self.job_urls = self.get_jobs_details_href(job_url_elements)
+        
+        while len(self.job_urls) < len(self.job_titles):
+            job_url = self.url + "#" + str(self.job_count)
+            self.job_urls.append(job_url)
+            self.job_count += 1
 
         self.format_data()
         
@@ -47,10 +54,8 @@ class britishcouncilScraper(BS4Scraper):
         """
         Iterate over all job details and send to the create jobs dictionary.
         """
-        for job_title in self.job_titles:
-            job_url = self.url + "#" + str(self.job_count)
+        for job_title, job_url in zip(self.job_titles, self.job_urls):
             self.create_jobs_dict(job_title, job_url, "România", ["Bucuresti", "Cluj-Napoca", "Timisoara", "Iasi"])
-            self.job_count += 1
 
 if __name__ == "__main__":
     britishcouncil = britishcouncilScraper()
