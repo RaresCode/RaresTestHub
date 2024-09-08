9b43ee4e2e4f2236dee1f67af6a943b8c16c125d
Rarescode <irimusrares7@gmail.com>
2024-09-08 14:55:57 +0300

    Added user agent to update

--- tests/TestJobsValidator/api_utils/peviitor_update.py
+++ tests/TestJobsValidator/api_utils/peviitor_update.py
@@ -31,7 +31,8 @@ class UpdateAPI:
         })
         
         post_header = {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
         }
 
         self.access_token = requests.request("POST", "https://api.laurentiumarian.ro/get_token", headers=post_header, data=payload).json()['access']
