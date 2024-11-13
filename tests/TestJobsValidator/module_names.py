ba5e8f37db783dcf4b34d82af8e359c9a91ec91d
RoxiPorime <roxana.porime03@gmail.com>
2024-11-13 19:34:47 +0200

    Update module_names.py

--- tests/TestJobsValidator/module_names.py
+++ tests/TestJobsValidator/module_names.py
@@ -1,5 +1,34 @@
 module_names = [
+            'Bosch',
+            'Genpact',
             'cargus',
             'artvertising',
+            'aeroportoradea',
+            'affidearomania',
+            #'allengra",
+            'alumil',
+            'anahr',
+            'apavital',
+            'arcadiamedical',
+            'artvertising',
+            'atpgroup',
+            'autoclass',
+            'autonet',
+            'autosiena',
+            'avaelgo',
+            'bakertilly',
+            'basware',
+            'bbcollection',
+            'beenear',
+            'berg',
+            'bioclinica',
+            'bitsoftware',
+            'bras',
+            'brillio',
+            'britishcouncil',
+            
+            
+            
+            
             # Add more module names as needed
             ]
